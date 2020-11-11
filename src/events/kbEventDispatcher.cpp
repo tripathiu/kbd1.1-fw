61bbd4a8067651738d0b4afb24120580206b2ff8
Utkarsh Tripathi <tri.utkarsh7596@gmail.com>
2020-11-12 02:09:44 +0530

    Move to CMake

---
+++ src/events/kbEventDispatcher.cpp
@@ -0,0 +1,21 @@
+#include "kbEventDispatcher.h"
+
+namespace kb {
+
+bool EvtDispatcher::addToQueue(Event evt) {
+  if (evt.getEvtType() == KeyDown) {
+  }
+  // Debug::print("Key Down");
+  else if (evt.getEvtType() == KeyRelease) {
+  }
+  // Debug::print("Key Release");
+  else if (evt.getEvtType() == KeyHold) {
+  }
+  // Debug::print("Key Hold");
+  else {
+  }
+  // Debug::print("Unrecognized Event");
+  return true;
+}
+
+}  // namespace kb
